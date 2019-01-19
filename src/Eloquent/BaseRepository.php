adddec075d26c5bbce8ee28140e4fb63afe2fb3e
aruberutoChan <albertoguerrerogarcia@gmail.com>
2019-01-19 13:08:44 +0100

    Repository use block cleaned

--- src/Eloquent/BaseRepository.php
+++ src/Eloquent/BaseRepository.php
@@ -2,16 +2,13 @@
 namespace Aruberuto\Repository\Eloquent;
 
 use Closure;
-use Exception;
 use Illuminate\Container\Container as Application;
-use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Aruberuto\Repository\Traits\ComparesVersionsTrait;
 use Aruberuto\Repository\Contracts\RepositoryCriteriaInterface;
 use Aruberuto\Repository\Contracts\RepositoryAncestorCriteriaInterface;
-use Aruberuto\Repository\Contracts\AncestorCriteriaInterface;
 
 use Aruberuto\Repository\Contracts\RepositoryInterface;
 use Aruberuto\Repository\Events\RepositoryEntityCreated;
