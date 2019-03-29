f6175217e65a55d06c4a1eec893325a60fd8d10b
aruberutochan <albertoguerrerogarcia@gmail.com>
2019-03-29 02:31:26 +0100

    Optimized download multi files

--- src/Traits/HasMediaControllerTrait.php
+++ src/Traits/HasMediaControllerTrait.php
@@ -2,9 +2,15 @@
 
 namespace Aruberuto\Repository\Traits;
 
-use Spatie\MediaLibrary\Models\Media;
 use Illuminate\Http\Request;
+use Chumper\Zipper\Facades\Zipper;
+use Illuminate\Support\Facades\File;
 use Spatie\MediaLibrary\MediaStream;
+use Spatie\MediaLibrary\Models\Media;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Str;
+
 trait HasMediaControllerTrait
 {
     public function download(Media $media, $id)
@@ -13,12 +19,46 @@ trait HasMediaControllerTrait
         return $mediaItem->getFullUrl();
     }
     
-    public function multiDownload(Media $media, Request $request) {
+    public function _multiDownload(Media $media, Request $request) {
 
         $medias = $media->findMany($request->get('ids', []));
         $mediaZip = MediaStream::create($request->get('name', 'my-files') . '.' . $request->get('extension', 'zip'))->addMedia($medias);
         return $mediaZip;
 
+    }
+
+    public function multiDownload(Media $media, Request $request)
+    {
+        $medias = $media->findMany($request->get('ids', []));
+        $mediasToZip = [];
+        foreach($medias as $media) {
+            $mediasToZip[] = $media->getPath();
+        }
+
+        // die(print_r($mediasToZip));
+        
+
+        // $headers = ["Content-Type"=>"application/zip"];
+        
+
+        $fileName = $request->get('name', 'my-files') . '.' . $request->get('extension', 'zip'); // name of zip
+        $filePath = public_path('/download/'.$fileName. Str::random());
+        $zip = Zipper::make($filePath) //file path for zip file
+                ->add($mediasToZip)->close(); //files to be zipped
+
+        $file = File::get( $filePath);
+        $type = File::mimeType( $filePath);
+
+        $response = Response::make($file, 200);
+        $response->header("Content-Type", $type);
+
+        return $response;
+
+        // $headers = array('Content-Type' => File::mimeType($filePath));
+        // return Storage::url($filePath);
+        // public_url()
+
+        // return response()->download($filePath, $fileName, $headers);
     }
 
 }
